d849a06a93deb42aec7134e0708bfa90e3f8f804
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2023-10-05 13:56:56 +0000

    datatypes

---
+++ _01_basics/_2_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as the newer version
+
+// code readability should be high
+
+// alert(4+4) // we are using nodejs, not browser
+
+let name="bhatia"
+let rollNum=488
+let pass=true
+let id;
+
+/* DATATYPES
+ number  => 2 to the power 53(something)
+ bigint
+ string => ""/''
+ boolean => true/false
+ null => standalone value
+ undefined 
+ symbol => unique
+ */
+
+// objects
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
