3bc024c6f561508ab2d7070b70d511a09f3398d0
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2023-10-05 13:25:40 +0000

    variables done

---
+++ _01_basics/_1_variables.js
@@ -0,0 +1,18 @@
+const nameId = 123
+let studentId= "45781"
+var course="B.Pharmacy"
+duration="4 years"
+
+// undefined value 
+let batchName
+
+// name = 487 not allowed
+studentId="8888"
+course="B.tech."
+duration="4 Years"
+console.table([nameId,studentId,course,duration,batchName])
+
+// Two ways to define variable in javascript - var, let ---> use let 
+
+// PREFER TO NOT use var 
+// because its issue in block scope and functional scope 
