0c328ef9db3cc3275444ecdcbe4ba40fa603835f
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2023-10-07 06:37:08 +0000

    conversion of datatypes

---
+++ _01_basics/_3_conversionOpertaions.js
@@ -0,0 +1,27 @@
+let score=false
+let result
+
+/*
+console.log(typeof (score))
+result=Number(score)
+console.log(typeof result)
+console.log(result);
+*/
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1; false => 0
+
+
+let loggedIn="lol"
+let booleanValue=Boolean(loggedIn)
+// console.log(booleanValue)
+
+// 0 => false & 1 => True
+// "" => false & "value" => true
+
+let num=69
+let stringNum=String(num)
+console.log(stringNum);
+console.log(typeof stringNum);
+
