d375a94aa13e9b891339b24dee8a3f77f5626c25
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2023-10-09 17:42:14 +0000

    strict equality operator

--- _01_basics/_4_comparison.js
+++ _01_basics/_4_comparison.js
@@ -11,8 +11,14 @@
 
 // console.log("2" >1 );
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 // The reason is that an equality check == and comparisons <,>,<=,>= works differently.
 // comparisons convert null to a number,treating it as 0.That's why null>=0 is true and null>0 is false
+
+// === strict equality operator
+console.log(2 === true);//false
+console.log(false === true);//false
+console.log(+0 === -0);//t
+console.log("hlo" === "hlo");//t
