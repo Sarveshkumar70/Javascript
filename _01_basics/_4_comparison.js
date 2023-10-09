1e941910a5957f781d14bba49a08c279c4adb20b
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2023-10-09 17:26:42 +0000

    Comparison of datatypes

---
+++ _01_basics/_4_comparison.js
@@ -0,0 +1,18 @@
+// simple but twist arrives when we compare values of two different datatypes, it gives unpredictable results
+
+// console.log(4 > 1);
+// console.log(4 >= 1);
+// console.log(4 <= 1);
+// console.log(4 == 1);
+// console.log(4 != 1);
+
+
+// Avoid below type of comparisons------->
+
+// console.log("2" >1 );
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// The reason is that an equality check == and comparisons <,>,<=,>= works differently.
+// comparisons convert null to a number,treating it as 0.That's why null>=0 is true and null>0 is false
