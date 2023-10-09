f059313c09fc1dde09875bcdd6d55aff07f2a5eb
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2023-10-09 13:48:38 +0000

    operations

--- _01_basics/_3_conversionOpertaions.js
+++ _01_basics/_3_conversionOpertaions.js
@@ -20,12 +20,38 @@ let booleanValue=Boolean(loggedIn)
 // 0 => false & 1 => True
 // "" => false & "value" => true
 
-let num=69
-let stringNum=String(num)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let num=69
+// let stringNum=String(num)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
 
 // *************** OPERATIONS **************
+let num1=45
+let num2=4
+// console.log(45-2);
+// console.log(45+2);
+// console.log(45*2);
+// console.log(45/2);
+// console.log(25**2);
 
 
+let str1="Hello"
+let str2=" ji"
+let str3=str1+str2;
+// console.log(str3);
+
+// Read ECMAscript type conversion
+// link -> // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+3+"2");
+// console.log("1"+3+2);
+// console.log(1+4+"2"+6+6);
+
+// Read prefix/postfix operations
+let scoreCounter=1000
+scoreCounter++;
+console.log(scoreCounter);
 
