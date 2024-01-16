baaae8155ccccf6b7b6ba9e6c20ad9e6e3ea947c
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2024-01-16 18:20:42 +0000

    IIFE concept

--- _03_basics/04_iife.js
+++ _03_basics/04_iife.js
@@ -1,11 +1,15 @@
-// IIFE -> Immediately Invoked Function Expressions
+// IIFE -> Immediately Invoked Function Expressions or also known as Self-Executing Anonymous Function
 
 // why to use IIFE? -> 
 // 1. Avoiding Global Scope Pollution
 // 2.  Executing a function immediately after its creation
 
-
 // syntax -> (function definition)(for calling function, even we can pass arguments);
+
+// IIFE contains two major parts:
+// The first is the anonymous function with lexical scope enclosed within the Grouping Operator (). This prevents accessing variables within the IIFE idiom as well as polluting the global scope.
+// The second part creates the immediately invoked function expression () through which the JavaScript engine will directly interpret the function.
+
 
 // Named IIFE
 (function connectionWithDB(){
