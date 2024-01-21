8297a48b3b8483828805fd0df4bb83a812d9017e
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2024-01-21 12:43:33 +0000

    while & do-while loop

---
+++ _05_iterations/02_while_dowhile_loops.js
@@ -0,0 +1,27 @@
+// while & do-while loop
+
+
+// let index = 1
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index * 2
+// }
+
+
+let myArray = ["One piece", "Monster", "Charlotte", "lookism"]
+
+let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value of array at index ${arr} is ${myArray[arr]}`);
+//     arr = arr +1
+// }
+
+
+
+// do-while loop 
+// [statement is always executed once before the condition is checked.]
+let i = 6
+// do {
+//     console.log("value of i is " + i);
+//     i++
+// } while (i<=5);
