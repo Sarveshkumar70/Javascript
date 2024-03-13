e6ec220f43dc4f8bcb6f373736e166fa6f9b0112
Sarveshkumar70 <113454567+Sarveshkumar70@users.noreply.github.com>
2024-03-13 17:14:20 +0530

    Update promise.js

--- _09_advance_one/promise.js
+++ _09_advance_one/promise.js
@@ -117,3 +117,16 @@
 // getUserData()
 
 
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(
+     (response) => {
+        return response.json()
+    }
+)
+.then( (data) => {
+    console.log(data);
+})
+.catch((error) =>{
+    console.log(error);
+})
+
